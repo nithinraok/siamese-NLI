272ec4462eeb9554520bd33ed6341f9c1220afba
Nithin Rao <nithinrao.koluguri@gmail.com>
2019-11-30 14:02:17 -0800

    transformer data

--- SNLI_data.py
+++ SNLI_data.py
@@ -34,4 +34,37 @@ class SNLI_dataset(Dataset):
             y = torch.tensor(0,dtype=torch.float32)
             return text,hyp,y
     
+
+
+class SNLI_TransDataset(Dataset):
+    def __init__(self,split):
+        super().__init__()
+
+        PATH1 = osp.join(ROOT,'data',split,'contradiction.txt')
+        PATH2 = osp.join(ROOT,'data',split,'entailment.txt')
+
+        with open(PATH1,'r') as f:
+            self.contra = f.readlines()
+        
+        with open(PATH2,'r') as f:
+            self.entail = f.readlines()
+        
+
+    def __len__(self):
+        return (len(self.contra)+len(self.entail))//2
+    
+    def __getitem__(self,val):
+        index=val*2
+        if index<len(self.contra):
+            text = self.contra[index].strip()
+            hyp = self.contra[index+1].strip()
+            y = torch.tensor(1,dtype=torch.float32)
+            return text,hyp,y
+        else:
+            index=index-len(self.contra)
+            text = torch.from_numpy(self.entail[index])
+            hyp = torch.from_numpy(self.entail[index+1])
+            y = torch.tensor(0,dtype=torch.float32)
+            return text,hyp,y
+      
     
